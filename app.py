5f52ae7dbad829751fb5a749f129b509f75eb5d8
dilukshi2004 <dilukshiwarangana2004@gmail.com>
2026-05-17 00:41:34 +0530

    Adding app python file to iniitialise the app

---
+++ app.py
@@ -0,0 +1,15 @@
+from flask import Flask
+
+app = Flask(__name__)
+
+@app.route("/")
+def home():
+    return "<h1>Hello from Flask inside Dcoker!</h1>"
+
+
+@app.route("/health")
+def helath():
+    return {"status": "healthy"}
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=5000)
